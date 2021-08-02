c87711c7b025d0597ee04270981fedd8ad57fd4e
AdrianPC <adrianchooo18@gmail.com>
2021-08-02 15:45:20 -0500

    PrimerasVersiones

---
+++ Día 4 - Hidden Search/script.js
@@ -0,0 +1,7 @@
+const open_btn = document.querySelector('.btn');
+const close_btn = document.querySelector('.btn')
+const inputS = document.querySelector('.input');
+
+open_btn.addEventListener('click',()=>{
+    inputS.style.opacity=1
+})
