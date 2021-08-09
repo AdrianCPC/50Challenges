4c16fa9a67a966518fdb7da79385746445c69445
AdrianPC <adrianchooo18@gmail.com>
2021-08-09 15:17:14 -0500

    arreglo problema expanding cards

--- Dia 1-Expanding Cards/script.js
+++ Dia 1-Expanding Cards/script.js
@@ -1,24 +1,21 @@
 
 // ¿Cuál es la opción que sirve?
 
-//const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 //const panels = document.querySelector('.panel')
 //const panels = document.getElementById('panel')
 //const panels = document.getElementsByClassName('.panel')
 
-
-
 panels.forEach((panel) => {
     //console.log(panel)
     panel.addEventListener('click', () => {
-        
-        panel.classList.add('active')
-
-
+        removeActiveClasses();
+        panel.classList.add('active');
     })
-
 })
 
 function removeActiveClasses(){
-    
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
 }
