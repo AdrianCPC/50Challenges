f1bc621763992884f8f798137d4f97721ef3cf40
AdrianPC <adrianchooo18@gmail.com>
2021-08-11 17:53:04 -0500

    day5 establecido

--- Día 5 - Blurry Loading/script.js
+++ Día 5 - Blurry Loading/script.js
@@ -1,21 +1,24 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
-let load = 0
+let load = 0;
+let int = setInterval(blurring, 30);
 
-let int = setInterval(blurring, 30)
-
-function blurring() {
-    
-
-    if(){
-       
+// ESTABLECIENDO EL RANGO
+function blurring () {
+    load++;
+    if(load > 99){
+        clearInterval(int);
+        // console.log(load);
     }
 
-    loadText.innerText = 
-    loadText.style.opacity = 
-    bg.style.filter = 
+    // IMPLEMENTANDO EL CONTENIDO
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`;
 }
 
-   
- 
+// CREACION MAPA RANGO DE NUMEROS
+const scale  = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
