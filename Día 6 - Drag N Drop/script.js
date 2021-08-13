020a058eec726c211e29c06e601ed8306b993ece
AdrianPC <adrianchooo18@gmail.com>
2021-08-12 19:16:12 -0500

    estructura completa js y css, pero sin funcionar correctamente aun

--- Día 6 - Drag N Drop/script.js
+++ Día 6 - Drag N Drop/script.js
@@ -1,34 +1,53 @@
-const fill = document.querySelector('')
-const empties = document.querySelectorAll('')
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
 
-fill.addEventListener('dragstart', )
-fill.addEventListener('dragend', )
+//EVENTOS DE LISTENER
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
 
+//CICLOS DE ARRASTRE
 for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 
 }
 
+//FUNCION DRAG
 function dragStart() {
-
-    setTimeout()
+    this.className += 'hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+    // console.log('start');
 }
 
 function dragEnd() {
-    this.
+    this.className = 'fill';
+    // console.log('end');
 }
 
 function dragOver(e) {
+    e.preventDefault();
+    // console.log('1');
 
 }
 
 function dragEnter(e) {
+    e.preventDefault();
+    this.className += 'hovered';
+    // console.log('2');
 
 }
 
 function dragLeave() {
+    this.className ='empty';
+    // console.log('3');
 
 }
 
 function dragDrop() {
+    this.className = 'empty';
+    this.appendChild
+    // console.log('4');
 
 }
