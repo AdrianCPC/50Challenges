89b24099c360832c2b0341b5a38105607bce7cf9
AdrianPC <adrianchooo18@gmail.com>
2021-08-13 17:01:01 -0500

    segunda manera de configurar las funciones del drag

--- Día 6 - Drag N Drop/script.js
+++ Día 6 - Drag N Drop/script.js
@@ -6,48 +6,51 @@ fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
 //CICLOS DE ARRASTRE
-for(const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-
+empties.forEach(empty => {
+     empty.addEventListener('dragover', dragOver);
+     empty.addEventListener('dragenter', dragEnter);
+     empty.addEventListener('dragleave', dragLeave);
+     empty.addEventListener('drop', dragDrop);
+ })
+//FUNCIONES DE DRAG O AGARRAR
+function dragOver(){
+    
 }
 
 //FUNCION DRAG
-function dragStart() {
-    this.className += 'hold';
-    setTimeout(() => (this.className = 'invisible'), 0);
-    // console.log('start');
-}
+// function dragStart() {
+//     //this.className += 'hold';
+//     // setTimeout(() => (this.className = 'invisible'), 0);
+//     console.log('start');
+// }
 
-function dragEnd() {
-    this.className = 'fill';
-    // console.log('end');
-}
+// function dragEnd() {
+//     // this.className = 'fill';
+//     console.log('end');
+// }
 
-function dragOver(e) {
-    e.preventDefault();
-    // console.log('1');
+// function dragOver(e) {
+//     e.preventDefault();
+//     // console.log('1');
 
-}
+// }
 
-function dragEnter(e) {
-    e.preventDefault();
-    this.className += 'hovered';
-    // console.log('2');
+// function dragEnter(e) {
+//     e.preventDefault();
+//     this.className += 'hovered';
+//     // console.log('2');
 
-}
+// }
 
-function dragLeave() {
-    this.className ='empty';
-    // console.log('3');
+// function dragLeave() {
+//     this.className ='empty';
+//     // console.log('3');
 
-}
+// }
 
-function dragDrop() {
-    this.className = 'empty';
-    this.appendChild
-    // console.log('4');
+// function dragDrop() {
+//     this.className = 'empty';
+//     this.append(fill);
+//     // console.log('4');
 
-}
+// }
