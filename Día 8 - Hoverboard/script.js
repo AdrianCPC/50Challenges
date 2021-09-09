723de99cbfd2dcfd3f59ce9ff187191bff2d726e
AdrianPC <adrianchooo18@gmail.com>
2021-09-08 22:22:52 -0500

    final day8

--- Día 8 - Hoverboard/script.js
+++ Día 8 - Hoverboard/script.js
@@ -1,29 +1,29 @@
-const container = document.getElementById('')
-const colors = ['', '', '', '', '']
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
-for(let i = 0; i < SQUARES; ) {
-    const square = document.createElement('')
-    square.classList.add('')
+for(let i = 0; i < SQUARES; i++ ) {
+    const square = document.createElement('div')
+    square.classList.add('square')
 
-    square.addEventListener('', () => setColor(square))
+    square.addEventListener('mouseover', () => setColor(square))
 
-    square.addEventListener('', () => removeColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
 
     container.appendChild(square)
 }
 
 function setColor(element) {
    const color = getRandomColor()
-   element.style.background = 
-   element.style.boxShadow = ``
+   element.style.background = color 
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
-   element.style.background = '#'
-   element.style.boxShadow = ''
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
 }
 
 function getRandomColor() {
-    return colors[Math.floor()]
+    return colors[Math.floor(Math.random() * colors.length)]
 }
