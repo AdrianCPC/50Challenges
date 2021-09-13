18283bd3710f6fb08e9a56965299687bd50fcb5c
AdrianPC <adrianchooo18@gmail.com>
2021-09-13 12:17:40 -0500

    day13 finished

--- Día 13 - Live User Filter/script.js
+++ Día 13 - Live User Filter/script.js
@@ -3,30 +3,37 @@ const filter = document.getElementById('filter')
 const listItems = []
 
 getData()
-
+//Filter with input for search people
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
-function getData() {
-    const res = 
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=50')
 
-    const {  } = json()
+    const {results} = await res.json()
+    //console.log(data)
 
     // Clear result
     result.innerHTML = ''
 
-    (user => {
+    results.forEach(user => {
         const li = document.createElement('li')
 
         listItems.push(li)
 
+        //Printing users on html
         li.innerHTML = `
+        <img src="${user.picture.large}" alt="${user.name.first}"
+        <div class="user-info">
+             <h4>${user.name.first} ${user.name.last}</h4>
+             <p>${user.location.city}, ${user.location.country}</p>
+        </div>
  
         `
 
         result.appendChild(li)
     })
 }
-
+//Function for fiter people
 function filterData(searchTerm) {
     listItems.forEach(item => {
         if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
