c3c18e067b34e867915072a5d46e29897a75d9e6
AdrianPC <adrianchooo18@gmail.com>
2021-09-13 13:03:02 -0500

    day14 finished

--- Día 14 - Button Ripple Effect/script.js
+++ Día 14 - Button Ripple Effect/script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.ripple')
 
+//Events listeners
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
         const x = e.clientX
@@ -17,6 +18,18 @@ buttons.forEach(button => {
         const yInside = y - buttonTop
 
         //console.log(xInside,yInside);
+
+        //Renderizing button and making
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        //Remove effects - time
+
+        setTimeout(() => circle.remove(), 500)
 
     })
 })
