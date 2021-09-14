225ed08c7b7bd4b785e4b175ca1d622aa806f8d9
AdrianPC <adrianchooo18@gmail.com>
2021-09-14 11:44:10 -0500

    day20 script review yet

--- Día 20 - Doble click Heart/script.js
+++ Día 20 - Doble click Heart/script.js
@@ -1,22 +1,25 @@
 const loveMe = document.querySelector('.loveMe')
 const times = document.querySelector('#times')
 
+
+//Start variables
 let clickTime = 0
 let timesClicked = 0
 
-/*  Probar 
+/*  Test
 loveMe.addEventListener('dblclick', (e) => {
     console.log('123')
 } ) */
 
 
+//Events click on loveMe
 loveMe.addEventListener('click', (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
         //console.log(clickTime);
     } else {
         if((new Date().getTime() - clickTime) < 800) {
-            //createHeart(e)
+            createHeart(e)
             //console.log(123);
             clickTime = 0
         } else {
@@ -25,7 +28,32 @@ loveMe.addEventListener('click', (e) => {
     }
 })
 
-
+//Making heart on card
 const createHeart = (e) => {
-  
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+//Directions
+    const x = e.clientX
+    const y = e.clientY
+    //console.log(x,y)
+
+
+    const leftOffset = e.target.OffsetLeft
+    const topOffset = e.target.OffsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+    //console.log(xInside, yInside)
+
+    //Styles Heart
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(() => heart.remove(), 1000)
 }
