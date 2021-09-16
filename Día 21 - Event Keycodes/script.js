c9b1f5e0ede0e131bc785f7ee9adec622803e448
AdrianPC <adrianchooo18@gmail.com>
2021-09-15 20:14:27 -0500

    day 21 finished

--- Día 21 - Event Keycodes/script.js
+++ Día 21 - Event Keycodes/script.js
@@ -1,7 +1,24 @@
 const insert = document.getElementById('insert')
 
+//Event keyboard
 window.addEventListener('keydown', (event) => {
-  console.log(e);
+  //console.log(e);
+  insert.innerHTML = `
+  <div class="key">
+  ${event.key === '' ? 'Space' : event.key}
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+`
 })
 
 
