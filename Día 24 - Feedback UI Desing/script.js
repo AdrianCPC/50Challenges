1fdea9fac35240139c386a460b7a1d7f168df1c6
AdrianPC <adrianchooo18@gmail.com>
2021-09-15 21:43:52 -0500

    finished day24

--- Día 24 - Feedback UI Desing/script.js
+++ Día 24 - Feedback UI Desing/script.js
@@ -1,25 +1,33 @@
-const ratings = document.querySelectorAll('.raing')
-const ratingsContainer = document.querySelector('.-container')
-const sendBtn = document.querySelector('#')
-const panel = document.querySelector('#')
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
-
+//Events listener for click
 ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
         selectedRating = e.target.nextElementSibling.innerHTML
+        // console.log(selectedRating)
     }
 })
-
-sendBtn.addEventListener('', (e) => {
+//Event click send
+sendBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
-
+    <i class="fas fa-heart"></i>
+    <strong>Thnak you!</strong>
+    <br>
+    <strong>Feedback : ${selectedRating}</strong>
+    <p>We'll use feedback to improve our customer support</p>
     `
 })
 
+//Remove active classes
+
 function removeActive() {
-{
+    for(let i = 0; i < ratings.length; i++) {
         ratings[i].classList.remove('active')
     }
 }
