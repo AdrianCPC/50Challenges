d63482b0dce9a95001cdbe260bc10b1fa39e87a4
AdrianPC <adrianchooo18@gmail.com>
2021-09-15 20:33:36 -0500

    day22 finished

--- Día 22 - Mobile Nav Navigation/script.js
+++ Día 22 - Mobile Nav Navigation/script.js
@@ -1,18 +1,29 @@
-const contents = document.querySelectorAll('')
-const listItems = document.querySelectorAll('')
+//const contens and items
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
 
+
+//Events listener
 listItems.forEach((item, idx) => {
-    item.addEventListener('', () => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+        
+//show the correct content
+        item.classList.add('active')
+        contents[idx].classList.add('show')
 
-        item.classList.add('')
     })
 })
 
-function hideAllContents() {
 
+//Functions for remove items and contens when click
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
 }
 
 
 function hideAllItems() {
+    listItems.forEach(item => item.classList.remove('active'))
  
 }
